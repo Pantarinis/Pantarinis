96a2315ad00269215bc730c7b49d5476b3de8b47
Tein van der Lugt <teinvdlugt@gmail.com>
2015-10-20 10:25:57 +0200

    Use page.js instead of <more-routing>

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+var app = document.querySelector("#app");
 
+page("/", function() {
+  app.route = 0;
+});
+page("/meedoen", function() {
+  app.route = 1;
+});
+page("/leden", function() {
+  app.route = 2;
+});
+page("/fotos", function() {
+  app.route = 3;
+});
+
+page({ hashbang: true});
